e37e7c373d0a3a08c904e83c133b54e33e391776
denmercs <denmercs@gmail.com>
2020-05-15 23:38:00 -0500

    working on the passport js

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,8 +1,27 @@
 const User = require("../models/user");
+const jwt = require("jwt-simple");
+const config = require("../config");
+
+function tokenForUser(user) {
+  const timestamp = new Date().getTime();
+  return jwt.encode(
+    {
+      sub: user.id,
+      iat: timestamp,
+    },
+    config.secret
+  );
+}
 
 exports.signup = function (req, res, next) {
   const email = req.body.email;
   const password = req.body.password;
+
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ error: "You must provide email and password" });
+  }
 
   // see if a user with the given email exists
   User.findOne({ email: email }, function (err, existingUser) {
@@ -26,7 +45,7 @@ exports.signup = function (req, res, next) {
       }
 
       // respond to request indicating the user was created
-      res.json({ success: true });
+      res.json({ token: tokenForUser(user) });
     });
   });
 };
